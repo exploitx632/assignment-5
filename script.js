ac247d76130cf958a1ea1ae4ebd54d7fcb448c78
Md Shaon Rana <142339875+exploitx632@users.noreply.github.com>
2025-08-28 18:42:11 +0800

    added get element function with id and class

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// get element with id and class 
+
+function getElementWithId(id){
+    const element = document.getElementById(id);
+    return element;
+}
+function getElementWithClass(Class){
+    const element = document.getElementsByClassName(Class);
+    return element;
+}
